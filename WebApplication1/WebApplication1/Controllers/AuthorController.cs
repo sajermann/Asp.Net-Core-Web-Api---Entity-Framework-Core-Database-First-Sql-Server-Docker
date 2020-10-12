34253844a8b9570c23217a98d73630d6373d99eb
Bruno Sajermann <contato@sajermann.com>
2020-10-12 15:55:41 -0300

    Video 1 complete

---
+++ WebApplication1/WebApplication1/Controllers/AuthorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        masterContext context = new masterContext();
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            //Get author by id
+            //var t = context.Author.Where(author => author.AuthorId == 1).ToList();
+
+            //Get all authors
+            //var t = context.Author.ToList();
+
+            //Add a author
+            //Author author = new Author();
+            //author.FirstName = "Bruno";
+            //author.LastName = "Sajermann";
+            //context.Author.Add(author);
+            //context.SaveChanges();
+
+            return context.Author.Where(author => author.FirstName == "Bruno").ToList();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Paginas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Paginas
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime? Data { get; set; }
+    }
+}
